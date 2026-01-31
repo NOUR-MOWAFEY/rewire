a3b71f41ebf4cc3df9c302ed0f576332d1d0a30c
Nour Mowafey <nourmowafey82@gmail.com>
2026-01-31 22:17:28 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/rewire/rewire/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rewire.rewire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ReWire());
+}
+
+class ReWire extends StatelessWidget {
+  const ReWire({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
